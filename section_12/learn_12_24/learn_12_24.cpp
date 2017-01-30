1be3758def28d5947d17ad00f97c174b947bd001
Токарь Тимур Михайлович <Tokar@giprosib.ru>
2017-01-30 12:52:45 +0700

    del section_12

--- section_12/learn_12_24/learn_12_24.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <new>
-
-int main(int argc, char *argv[]){
-    char *str = new char[10];
-    std::cin >> str;
-    std::cout << str <<std::endl;
-    delete [] str;
-}
