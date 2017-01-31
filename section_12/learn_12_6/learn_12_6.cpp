b3e8114db09fded306278eb719b81ab826861027
Токарь Тимур Михайлович <Tokar@giprosib.ru>
2017-01-31 11:19:26 +0700

    section 12 learn 12.6

---
+++ section_12/learn_12_6/learn_12_6.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <new>
+
+std::vector<int>* vec_create();
+std::vector<int>* in_ivec(std::vector<int>* ivec);
+void print(std::vector<int>* ivec);
+
+int main(int argc, char *argv[]){
+	std::vector<int>* ivec = vec_create();
+	in_ivec(ivec);
+	print(ivec);
+}
+
+std::vector<int>* vec_create(){
+	std::vector<int>* ivec = new std::vector<int>;
+	return ivec;
+}
+
+std::vector<int>* in_ivec(std::vector<int>* ivec){
+	int num;
+	for(std::vector<int>::size_type i = 0; i < 3; ++i){
+		std::cin >> num;
+		ivec->push_back(num);
+	}
+	return ivec;
+}
+
+void print(std::vector<int>* ivec){
+	for(int i: *ivec)
+		std::cout << i << " " << std::flush;
+	delete ivec;
+}
