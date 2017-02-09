2f5ca4b21095825993c5dfae0d7384781b07f302
Токарь Тимур Михайлович <Tokar@giprosib.ru>
2017-02-09 11:39:35 +0700

    mod learn 13.5 and 13.8

--- section_13/learn_13_5/HasPtr.cpp
+++ section_13/learn_13_5/HasPtr.cpp
@@ -1,9 +1,9 @@
 #include "HasPtr.h"
 
-HasPtr& HasPtr::operator=(const HasPtr& rhs){
-	ps = rhs.ps;
+HasPtr::HasPtr(const HasPtr& rhs){
+	std::string *pstmp(rhs.ps);
+	ps = new std::string(*pstmp);
 	i = rhs.i;
-	return *this;
 }
 
 std::ostream& HasPtr::print(std::ostream &os){
--- section_13/learn_13_8/HasPtr.cpp
+++ section_13/learn_13_8/HasPtr.cpp
@@ -1,12 +1,14 @@
 #include "HasPtr.h"
 
 HasPtr::HasPtr(const HasPtr& rhs){
-	ps = rhs.ps;
+	std::string *pstmp(rhs.ps);
+	ps = new std::string(*pstmp);
 	i = rhs.i;
 }
 
-HasPtr& HasPtr::operator=(const HasPtr& rhs){
-	ps = rhs.ps;
+HasPtr& HasPtr::operator=(const HasPtr &rhs){
+	std::string *pstmp(rhs.ps);
+	ps = new std::string(*pstmp);
 	i = rhs.i;
 	return *this;
 }
